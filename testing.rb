2cb4a3b7fe5a76841d26046d51e6d59945a7bc5c
Mercedes Delin <mercedesgdelin@gmail.com>
2019-08-20 21:58:19 +0000

    Done.

---
+++ testing.rb
@@ -0,0 +1,11 @@
+def speak_to_grandma (you_say)
+  
+  if you_say == "I LOVE YOU GRANDMA!"
+    return "I LOVE YOU TOO PUMPKIN!"
+  elsif  you_say == you_say.upcase
+    return "NO, NOT SINCE 1938!"
+  else 
+    return "HUH?! SPEAK UP, SONNY!"
+  
+end
+end
